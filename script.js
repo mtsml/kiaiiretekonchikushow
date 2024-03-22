5f53f17ff6a20891e081a274482b090870fc2004
matsu <50922910+mtsml@users.noreply.github.com>
2024-03-22 22:25:12 +0900

    refactor

--- script.js
+++ script.js
@@ -1,44 +1,46 @@
-const TEXT_HELLO_MEG = "ハロめぐー！";
-const HELLO_MEG_URL_PARAM = '%EF%BC%BF%E4%BA%BA%E4%BA%BA%E4%BA%BA%E4%BA%BA%E4%BA%BA%E4%BA%BA%E4%BA%BA%E4%BA%BA%EF%BC%BF%0A%EF%BC%9E%E3%80%80%E3%83%8F%E3%83%AD%E3%82%81%E3%81%90%E3%83%BC%EF%BC%81%E3%80%80%EF%BC%9C%0A%EF%BF%A3Y%5EY%5EY%5EY%5EY%5EY%5EY%EF%BF%A3%0A';
-const BIG_HELLO_MEG_URL_PARAM = '%E2%A0%80%E2%80%83%E2%96%A0%0A%E2%80%83%E2%96%A0%E2%80%83%E2%80%83%E2%80%83%E2%96%A0%0A%E2%80%83%E2%96%A0%E2%80%83%E2%80%83%E2%80%83%E2%96%A0%0A%E2%96%A0%E2%80%83%E2%80%83%E2%80%83%E2%80%83%E2%80%83%E2%96%A0%0A%E2%96%A0%E2%80%83%E2%80%83%E2%80%83%E2%80%83%E2%80%83%E2%80%83%E2%96%A0%0A%0A%E3%80%80%E2%96%A0%E2%96%A0%E2%96%A0%E2%96%A0%E2%96%A0%E2%96%A0%0A%E3%80%80%E2%96%A0%E3%80%80%E3%80%80%E3%80%80%20%20%E2%96%A0%0A%E3%80%80%E2%96%A0%E3%80%80%E3%80%80%E3%80%80%20%20%E2%96%A0%0A%E3%80%80%E2%96%A0%E2%96%A0%E2%96%A0%E2%96%A0%E2%96%A0%E2%96%A0%0A%0A%E2%80%83%E2%80%83%E2%80%83%E2%80%83%E2%80%83%E2%96%A0%0A%E2%80%83%E2%96%A0%E2%80%83%E2%80%83%E2%80%83%E2%96%A0%0A%E2%80%83%E2%96%A0%E2%96%A0%E2%96%A0%E2%80%83%E2%96%A0%E2%96%A0%E2%96%A0%0A%E2%80%83%E2%96%A0%E2%96%A0%E2%80%83%E2%80%83%E2%96%A0%E2%80%83%E2%96%A0%E2%96%A0%0A%E2%96%A0%E2%80%83%E2%96%A0%E2%80%83%E2%96%A0%E2%80%83%E2%80%83%E2%80%83%E2%96%A0%0A%E2%96%A0%E2%80%83%E2%80%83%E2%96%A0%E2%80%83%E2%80%83%E2%80%83%E2%96%A0%0A%E3%80%80%E2%96%A0%E2%96%A0%E2%80%83%E2%80%83%E2%80%83%E2%96%A0%0A%0A%E2%80%83%E2%80%83%E2%80%83%E2%80%83%E2%96%A0%0A%E2%80%83%E2%80%83%E2%96%A0%E2%96%A0%0A%E2%80%83%E2%96%A0%E2%96%A0%E2%80%83%E2%80%83%E2%96%A0%E2%80%83%E2%96%A0%0A%E2%96%A0%E2%80%83%E2%80%83%E2%80%83%E2%80%83%E2%80%83%E2%96%A0%0A%E2%96%A0%E2%96%A0%0A%E2%80%83%E2%80%83%E2%96%A0%E2%96%A0%0A%E2%80%83%E2%80%83%E2%80%83%E2%96%A0%E2%96%A0%0A';
+const TEXT_HELLOMEG = "ハロめぐー！";
+const HELLOMEG_URL_PARAM = '%EF%BC%BF%E4%BA%BA%E4%BA%BA%E4%BA%BA%E4%BA%BA%E4%BA%BA%E4%BA%BA%E4%BA%BA%E4%BA%BA%EF%BC%BF%0A%EF%BC%9E%E3%80%80%E3%83%8F%E3%83%AD%E3%82%81%E3%81%90%E3%83%BC%EF%BC%81%E3%80%80%EF%BC%9C%0A%EF%BF%A3Y%5EY%5EY%5EY%5EY%5EY%5EY%EF%BF%A3%0A';
+const BIG_HELLOMEG_URL_PARAM = '%E2%A0%80%E2%80%83%E2%96%A0%0A%E2%80%83%E2%96%A0%E2%80%83%E2%80%83%E2%80%83%E2%96%A0%0A%E2%80%83%E2%96%A0%E2%80%83%E2%80%83%E2%80%83%E2%96%A0%0A%E2%96%A0%E2%80%83%E2%80%83%E2%80%83%E2%80%83%E2%80%83%E2%96%A0%0A%E2%96%A0%E2%80%83%E2%80%83%E2%80%83%E2%80%83%E2%80%83%E2%80%83%E2%96%A0%0A%0A%E3%80%80%E2%96%A0%E2%96%A0%E2%96%A0%E2%96%A0%E2%96%A0%E2%96%A0%0A%E3%80%80%E2%96%A0%E3%80%80%E3%80%80%E3%80%80%20%20%E2%96%A0%0A%E3%80%80%E2%96%A0%E3%80%80%E3%80%80%E3%80%80%20%20%E2%96%A0%0A%E3%80%80%E2%96%A0%E2%96%A0%E2%96%A0%E2%96%A0%E2%96%A0%E2%96%A0%0A%0A%E2%80%83%E2%80%83%E2%80%83%E2%80%83%E2%80%83%E2%96%A0%0A%E2%80%83%E2%96%A0%E2%80%83%E2%80%83%E2%80%83%E2%96%A0%0A%E2%80%83%E2%96%A0%E2%96%A0%E2%96%A0%E2%80%83%E2%96%A0%E2%96%A0%E2%96%A0%0A%E2%80%83%E2%96%A0%E2%96%A0%E2%80%83%E2%80%83%E2%96%A0%E2%80%83%E2%96%A0%E2%96%A0%0A%E2%96%A0%E2%80%83%E2%96%A0%E2%80%83%E2%96%A0%E2%80%83%E2%80%83%E2%80%83%E2%96%A0%0A%E2%96%A0%E2%80%83%E2%80%83%E2%96%A0%E2%80%83%E2%80%83%E2%80%83%E2%96%A0%0A%E3%80%80%E2%96%A0%E2%96%A0%E2%80%83%E2%80%83%E2%80%83%E2%96%A0%0A%0A%E2%80%83%E2%80%83%E2%80%83%E2%80%83%E2%96%A0%0A%E2%80%83%E2%80%83%E2%96%A0%E2%96%A0%0A%E2%80%83%E2%96%A0%E2%96%A0%E2%80%83%E2%80%83%E2%96%A0%E2%80%83%E2%96%A0%0A%E2%96%A0%E2%80%83%E2%80%83%E2%80%83%E2%80%83%E2%80%83%E2%96%A0%0A%E2%96%A0%E2%96%A0%0A%E2%80%83%E2%80%83%E2%96%A0%E2%96%A0%0A%E2%80%83%E2%80%83%E2%80%83%E2%96%A0%E2%96%A0%0A';
 
-const helloMeg = (e) => {
+const hellomeg = (e) => {
   // 画像を揺らす
   e.classList.add("shake-img");
   setTimeout(() => {
     e.classList.remove("shake-img");
   }, 300);
 
-  const helloMegElement = document.createElement("div");
-  helloMegElement.textContent = TEXT_HELLO_MEG;
-  helloMegElement.classList.add("hello-meg");
+  const hellomegElement = document.createElement("div");
+  hellomegElement.textContent = TEXT_HELLOMEG;
+  hellomegElement.classList.add("hellomeg");
 
   // container の中心から外側に向かってランダムに飛び出す
   const randomAngle = Math.random() * 2 * Math.PI;
   const translateX = Math.cos(randomAngle) * 180;
   const translateY = Math.sin(randomAngle) * 180;
-  helloMegElement.style.setProperty("--translateX", translateX + "px");
-  helloMegElement.style.setProperty("--translateY", translateY + "px");
-  helloMegElement.style.setProperty("--startX", Math.cos(randomAngle) * 20 + "%");
-  helloMegElement.style.setProperty("--startY", Math.sin(randomAngle) * 20 + "%");
+  hellomegElement.style.setProperty("--translateX", translateX + "px");
+  hellomegElement.style.setProperty("--translateY", translateY + "px");
+  hellomegElement.style.setProperty("--startX", Math.cos(randomAngle) * 20 + "%");
+  hellomegElement.style.setProperty("--startY", Math.sin(randomAngle) * 20 + "%");
 
   const container = document.querySelector(".container");
-  container.appendChild(helloMegElement);
+  container.appendChild(hellomegElement);
 
   // animation 完了後に要素を削除する
   setTimeout(() => {
-    helloMegElement.remove();
+    hellomegElement.remove();
   }, 2000);
 }
 
 const startHellomegCount = (hellomegImgElement) => {
   let score = 0;
 
+  // onclick を上書きしてクリック時に score 加算処理を設定
   hellomegImgElement.onclick = () => {
     score++;
     document.getElementById("score").innerText = `${score} ハロめぐー！`;
-    helloMeg(hellomegImgElement);
+    hellomeg(hellomegImgElement);
   }
+
   // スタート時のクリックもカウントする
   hellomegImgElement.click(hellomegImgElement)
 
@@ -64,21 +66,21 @@ const startHellomegCount = (hellomegImgElement) => {
   }, 100);
 }
 
-const helloMegPostCreate = (e) => {
+const setHellomegTweetLink = (e) => {
   // URL作成
-  let url = createUrl();
+  const url = createUrl();
   // URL差し替え
-  e.href=url;
+  e.href = url;
 }
 
-function createUrl() {
+const createUrl = () => {
   // 0~99の整数をランダム生成
-  let randomNum = Math.floor( Math.random() * 100 );
+  const randomNum = Math.floor(Math.random() * 100);
 
   // 30%の確率でクソデカハロめぐツイート作成
   if (randomNum < 30) {
-    return "https://twitter.com/intent/tweet?text="+BIG_HELLO_MEG_URL_PARAM+"&url=https://kiaiiretekonchiku.show/";
+    return "https://twitter.com/intent/tweet?text="+BIG_HELLOMEG_URL_PARAM+"&url=https://kiaiiretekonchiku.show/";
   } else {
-    return "https://twitter.com/intent/tweet?text="+HELLO_MEG_URL_PARAM+"&url=https://kiaiiretekonchiku.show/";
+    return "https://twitter.com/intent/tweet?text="+HELLOMEG_URL_PARAM+"&url=https://kiaiiretekonchiku.show/";
   }
 }
