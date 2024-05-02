1cd44d3b377587edcf4c5040e43e32e73ff4da39
matsu <50922910+mtsml@users.noreply.github.com>
2024-05-02 17:51:09 +0900

    ツイートを修正

--- script.js
+++ script.js
@@ -409,6 +409,7 @@ const startHaromegu = (e) => {
       clearInterval(interval);
       container.style.display = "none";
       document.getElementById("result").style.display = "block";
+      document.getElementById("post").href = `https://twitter.com/intent/tweet?text=%23%E3%83%8F%E3%83%AD%E3%82%81%E3%81%90%E3%83%95%E3%82%A1%E3%82%A4%E3%82%A2%0D%0A%E3%83%8F%E3%83%AD%E3%82%81%E3%81%90%E3%82%92%E7%B4%A0%E6%97%A9%E3%81%8F%E3%81%A4%E3%81%AA%E3%81%92%E3%81%A6%E5%8F%8D%E5%B0%84%E7%A5%9E%E7%B5%8C%E3%82%92%E7%88%86%E4%B8%8A%E3%81%92%E3%81%97%E3%82%88%E3%81%86%E3%80%82%E7%A7%81%E3%81%8C10%E3%83%8F%E3%83%AD%E3%82%81%E3%81%90%E5%AE%8C%E6%88%90%E3%81%95%E3%81%9B%E3%82%8B%E3%81%AE%E3%81%AB%E3%81%8B%E3%81%8B%E3%81%A3%E3%81%9F%E6%99%82%E9%96%93%E3%81%AF%E2%80%A6%0D%0A%0D%0A${Math.floor(time / 10)}.${Math.floor(time % 10)}+%E7%A7%92%EF%BC%81%0D%0A&url=https://kiaiiretekonchiku.show/haromegu.html`;
       displayAdTweet();
 
       // timerElement の反映を待つために非同期実行する
