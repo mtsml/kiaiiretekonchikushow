154230524a3589290a27d0bb3d1cb4030a61c8c4
matsu <50922910+mtsml@users.noreply.github.com>
2024-09-22 03:23:57 +0900

    ws

--- src/pages/ws/script.js
+++ src/pages/ws/script.js
@@ -1,3 +1,25 @@
 const handleClick = async () => {
-  await fetch('/api/ws');
+  const url = new URL(window.location);
+  url.protocol = "wss";
+  url.pathname = "/api/ws";
+  await websocket(url);
 }
+
+let ws;
+
+const websocket = async (url) => {
+  ws = new WebSocket(url);
+
+  if (!ws) {
+    throw new Error("server didn't accept ws")
+  }
+
+  ws.addEventListener("open", () => {
+    console.log('Opened websocket');
+  })
+
+  ws.addEventListener("message", ({ data }) => {
+    const { count, tz, error } = JSON.parse(data)
+  })
+}
+
