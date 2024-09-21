2227c68e65597093c8754eaaebf3702eafd342d5
matsu <50922910+mtsml@users.noreply.github.com>
2024-09-22 03:10:56 +0900

    simple request

--- src/pages/ws/script.js
+++ src/pages/ws/script.js
@@ -1,22 +1,3 @@
-let ws;
-
-const websocket = async (url) => {
-  ws = new WebSocket(url);
-
-  if (!ws) {
-    throw new Error("server didn't accept ws")
-  }
-
-  ws.addEventListener("open", () => {
-    console.log('Opened websocket');
-  })
-
-  ws.addEventListener("message", ({ data }) => {
-    const { count, tz, error } = JSON.parse(data)
-  })
+const handleClick = async () => {
+  await fetch('/api/ws');
 }
-
-const url = new URL(window.location);
-url.protocol = "wss";
-url.pathname = "/ws";
-websocket(url);
