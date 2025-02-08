c36a6ec0b5c529847c6b0aba08d4405248608450
matsu <50922910+mtsml@users.noreply.github.com>
2025-02-09 08:17:09 +0900

    feat: additonal model

--- src/pages/odekake/script.js
+++ src/pages/odekake/script.js
@@ -20,3 +20,29 @@ function nextSlide() {
 function prevSlide() {
   showSlide(currentSlide - 1);
 }
+
+window.addEventListener("DOMContentLoaded", () => {
+  const params = new URL(document.location).searchParams;
+  const additionalModel = params.get("additional_model");
+  
+  switch (additionalModel) {
+    case "a1B2c3":
+      document.getElementById("a1B2c3").style.display = null;
+      break;
+    case "d4E5f6":
+      document.getElementById("d4E5f6").style.display = null;
+      break;
+    case "g7H8i9":
+      document.getElementById("g7H8i9").style.display = null;
+      break;
+    case "j0K1l2":
+      document.getElementById("j0K1l2").style.display = null;
+      break;
+    case "m3N4o5":
+      document.getElementById("m3N4o5").style.display = null;
+      break;
+    case "p6Q7r8":
+      document.getElementById("p6Q7r8").style.display = null;
+      break;
+    }
+});
