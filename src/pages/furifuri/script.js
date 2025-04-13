3c24c954c9e90b3931ca2485e2e191bba47d597f
matsu <50922910+mtsml@users.noreply.github.com>
2025-04-13 22:35:06 +0900

    update

--- src/pages/furifuri/script.js
+++ src/pages/furifuri/script.js
@@ -1,5 +1,5 @@
-let SHAKE_THRESHOLD = 15;
-let SHAKE_INTERVAL = 200;
+const SHAKE_THRESHOLD = 50;
+const SHAKE_INTERVAL = 200;
 let lastShakeTime = new Date().getTime();
 let shakeCount = 0
 let scoreElement, hellomegElement;
@@ -27,7 +27,7 @@ function startGame() {
       document.body.classList.remove("overflow-hidden");
       document.getElementById("description").style.display = "none";
       document.getElementById("result").style.display = "block";
-      // document.getElementById("post").href = `https://twitter.com/intent/tweet?text=%23%E3%83%8F%E3%83%AD%E3%82%81%E3%81%90%E3%82%AB%E3%82%A6%E3%83%B3%E3%83%88%0D%0A%E3%83%8F%E3%83%AD%E3%82%81%E3%81%90%E3%82%92%E9%80%A3%E6%89%93%E3%81%97%E3%81%A6%E3%82%B9%E3%82%AF%E3%82%B9%E3%83%86%E7%AD%8B%E3%82%92%E9%8D%9B%E3%81%88%E3%82%88%E3%81%86%E3%80%82%E7%A7%81%E3%81%AE10%E7%A7%92%E9%96%93%E3%81%AE%E9%80%A3%E6%89%93%E7%B5%90%E6%9E%9C%E3%81%AF%E2%80%A6%0D%0A%0D%0A${score}+%E3%83%8F%E3%83%AD%E3%82%81%E3%81%90%E3%83%BC%EF%BC%81%0D%0A&url=https://kiaiiretekonchiku.show/count/`;
+      document.getElementById("post").href = `https://twitter.com/intent/tweet?text=%23%E3%83%8F%E3%83%AD%E3%82%81%E3%81%90%E3%82%B7%E3%82%A7%E3%82%A4%E3%82%AF%0D%0A%E3%82%B9%E3%83%9E%E3%83%9B%E3%82%92%E3%83%95%E3%83%AA%E3%83%95%E3%83%AA%E2%99%AA%E7%A7%81%E3%81%AE%E7%B5%90%E6%9E%9C%E3%81%AF%E2%80%A6%0D%0A%0D%0A${shakeCount}+%E3%83%8F%E3%83%AD%E3%82%81%E3%81%90%E3%83%BC%EF%BC%81&url=https://kiaiiretekonchiku.show/shake/`;
       
       // timerElement の反映を待つために非同期実行する
       setTimeout(() => {
@@ -117,13 +117,3 @@ document.addEventListener('DOMContentLoaded', function() {
   document.getElementById("playable").style.display = null;
   document.getElementById("logo").onclick = startGame;
 });
-
-const hundleThresholdChange = (event) => {
-  SHAKE_THRESHOLD = Number(event.target.value);
-  document.getElementById('threshold-value').textContent = event.target.value;
-}
-
-const hundleIntervalChange = (event) => {
-  SHAKE_INTERVAL = Number(event.target.value);
-  document.getElementById('interval-value').textContent = event.target.value;
-}
