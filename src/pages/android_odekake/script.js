8c53ddaaaef81a23fb668d429a446645e3c8f8ae
matsu <50922910+mtsml@users.noreply.github.com>
2025-06-19 01:19:59 +0900

    Revert "remove: android_odekakeディレクトリを削除"
    This reverts commit f375046a3120765c3ab541b1080655e8ec462353.

---
+++ src/pages/android_odekake/script.js
@@ -0,0 +1,136 @@
+const models = [
+  {
+    src: 'kyun.glb',
+    alt: 'kyun 3D model',
+    twitterId: 'hisaka033'
+  },
+  {
+    src: 'puku.glb',
+    alt: 'puku 3D model',
+    twitterId: 'pine_nm'
+  },
+  {
+    src: 'akeomeg.glb',
+    alt: 'akeomeg 3D model',
+    twitterId: 'pine_nm'
+  },
+  {
+    src: 'jk.glb',
+    alt: 'jk 3D model',
+    twitterId: 'pine_nm'
+  },
+  {
+    src: 'hakuchu.glb',
+    alt: 'hakuchu 3D model',
+    twitterId: 'pine_nm'
+  },
+  {
+    src: 'doya.glb',
+    alt: 'doya 3D model',
+    twitterId: 'pine_nm'
+  },
+  {
+    src: 'plank.glb',
+    alt: 'plank 3D model',
+    twitterId: 'pine_nm'
+  },
+  {
+    src: 'low.glb',
+    alt: 'low 3D model',
+    twitterId: 'pine_nm'
+  },
+  {
+    src: 'hellomeg.glb',
+    alt: 'hellomeg 3D model',
+    twitterId: 'pine_nm'
+  },
+  {
+    src: 'ramen.glb',
+    alt: 'ramen 3D model',
+    twitterId: 'pine_nm'
+  },
+  {
+    src: 'aa.glb',
+    alt: 'aa 3D model',
+    twitterId: 'pine_nm'
+  },
+  {
+    src: 'katan.glb',
+    alt: 'katan 3D model',
+    twitterId: 'pine_nm'
+  },
+  {
+    src: 'universe.glb',
+    alt: 'universe 3D model',
+    twitterId: 'pine_nm'
+  },
+  {
+    src: 'damon.glb',
+    alt: 'damon 3D model',
+    twitterId: 'pine_nm'
+  },
+  {
+    src: 'stand_megu.glb',
+    alt: 'stand_megu 3D model',
+    twitterId: 'pine_nm'
+  },
+  {
+    src: 'stand_ruri.glb',
+    alt: 'stand_ruri 3D model',
+    twitterId: 'pine_nm'
+  },
+];
+
+let currentSlide = 0;
+
+function showSlide(index) {
+  const slider = document.getElementById('slider');
+  const totalSlides = slider.children.length;
+  if (index >= totalSlides) {
+    currentSlide = 0;
+  } else if (index < 0) {
+    currentSlide = totalSlides - 1;
+  } else {
+    currentSlide = index;
+  }
+  slider.style.transform = `translateX(-${currentSlide * 100}%)`;
+
+  const twitterId = models[currentSlide].twitterId;
+  const actionLabel = document.getElementById('action-label');
+  actionLabel.innerText = `イラスト：@${twitterId}`;
+  actionLabel.href = `https://twitter.com/${twitterId}`;
+  actionLabel.target = '_blank';
+}
+
+function nextSlide() {
+  showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+  showSlide(currentSlide - 1);
+}
+
+function initializeModelViewers() {
+  const slider = document.getElementById('slider');
+  slider.innerHTML = '';
+  
+  models.forEach((model) => {
+    const modelViewer = document.createElement('model-viewer');
+    modelViewer.src = model.src;
+    modelViewer.alt = model.alt;
+    modelViewer.setAttribute('camera-controls', '');
+    modelViewer.setAttribute('ar', '');
+    modelViewer.setAttribute('ar-modes', 'scene-viewer quick-look');
+    slider.appendChild(modelViewer);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  initializeModelViewers();
+  
+  const twitterId = models[currentSlide].twitterId;
+  const actionLabel = document.getElementById('action-label');
+  actionLabel.innerText = `イラスト：@${twitterId}`;
+  actionLabel.href = `https://twitter.com/${twitterId}`;
+  actionLabel.target = '_blank';
+});
