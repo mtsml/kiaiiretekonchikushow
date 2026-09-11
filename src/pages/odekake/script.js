4244eae7a6e26c1a255e29a040d3dd1dcd239675
matsu <50922910+mtsml@users.noreply.github.com>
2026-09-11 22:33:38 +0900

    chore: update package

--- src/pages/odekake/script.js
+++ src/pages/odekake/script.js
@@ -189,7 +189,8 @@ function initializeModelViewers() {
     modelViewer.src = model.src;
     // @ts-ignore modelViewer の型情報が存在しないため ignore
     modelViewer.alt = model.alt;
-    modelViewer.setAttribute('ios-src', model.src);
+    // iOS の Quick Look 用 USDZ は model-viewer に生成させる。
+    // GLB を ios-src に設定すると、Safari は AR ではなくファイルのダウンロードとして扱う。
     modelViewer.setAttribute('camera-controls', '');
     modelViewer.setAttribute('ar', '');
     if (isAndroid() && cameraArModeEnabled) {
